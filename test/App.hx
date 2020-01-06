f8dfa98501155a768bb87a5e5119575ba7d77578
Gábor Váradi <varadi83gabor@gmail.com>
2020-01-06 11:07:26 +0100

    rename project

--- test/App.hx
+++ test/App.hx
@@ -35,43 +35,25 @@ class App {
 
 			Admin.initializeApp({
 				credential: Admin.credential.cert(Json.parse(serviceAccountKey)),
-				storageBucket: "gs://devinflow-partner-icons/",
+				storageBucket: "gs://devinflow.appspot.com",
 				databaseURL: "https://devinflow.firebaseio.com"
 			});
 
-			// Admin.messaging().send(message).then(function(response){
-			//     // Response is a message ID string.
-			//     Node.console.log('Successfully sent message:', response);
-			// })
-			// .catchError(function(error){
-			// Node.console.log('Error sending message:', error);
-			// });
-
-			var partnerIcons:Bucket = Admin.storage().bucket("devinflow-partner-icons");
-			
-			partnerIcons.upload("test.jpg",{destination:"test/imageTEst2.jpg"}).then((response:UploadResponse)->{
-				trace(response);
-				var file:File =response[0];
-				trace(file);
-			});
-			// bucket.create().then(function(response) trace (response)).catchError(function(error) trace(error));
-			// DatabaseLogging.enable(true);
 			var database = Admin.database();
-			var users = database.ref("users/");
-			var partners = database.ref("partners/");
-			users.push().set({name:"Varadi GAbo6662"});
-			partners.push().set({name:"Y&R",foo:"BAR"});
-			partners.push().set({name:"Y&R2",foo:"BAR2",kaki:"hig"});
-			
-			
-			// trace(users.toJSON());
-			// Admin.database().ref("users").once(EventType.VALUE, function(snapshot) {
-				// trace(snapshot.key);
-			// });
 
-			// bucket.getFiles().then((response) -> trace(response)).catchError(function(error) {
-			// trace(error);
-			// });
+			var bucket:Bucket = Admin.storage().bucket();
+
+			bucket.upload("test.png", {destination: 'new-image.png'}).then((response:UploadResponse) -> {
+				var file:File = response[0];
+				file.makePublic().then((response:Array<Dynamic>) -> {
+					var storageRoot = 'https://storage.googleapis.com/';
+					var bucketName = 'devinflow.appspot.com/'; // CHANGE TO YOUR BUCKET NAME
+					var downloadUrl = storageRoot + bucketName + "new-image.png";
+					trace(downloadUrl);
+				});
+			}).catchError((error) -> {
+				trace(error);
+			});
 		});
 	}
 }
